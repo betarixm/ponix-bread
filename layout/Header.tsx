5a8249440fd4673fd5436f14ad0e0f71f416accb
beta <beta.underalpha@gmail.com>
2022-03-30 01:51:31 +0900

    (#8) Styling: Header

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Link from "next/link";
+import styles from "./Header.module.scss";
 
 interface HeaderProps {}
 
@@ -6,7 +8,27 @@ interface HeaderState {}
 
 class Header extends React.Component<HeaderProps, HeaderState> {
     render = () => {
-        return <header>Header</header>;
+        return (
+            <nav className={styles.nav}>
+                <div className={styles.container}>
+                    <div className={styles.inner}>
+                        <div className={styles.title}>
+                            <Link href={"/"}>Ponix Bread</Link>
+                        </div>
+                        <div className={styles.content}>
+                            <ul>
+                                <li>
+                                    <Link href={"/draw"}>뽑기</Link>
+                                </li>
+                                <li>
+                                    <Link href={"/collection"}>도감</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        );
     };
 }
 
