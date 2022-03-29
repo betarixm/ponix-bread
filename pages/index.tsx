158cfd0c3eab54c8b07e9c82a6809e75792d4f2b
beta <beta.underalpha@gmail.com>
2022-03-30 01:51:57 +0900

    (#5) Styling: Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,43 @@
 import React from "react";
+import Router from "next/router";
+import Image from "next/image";
 import { Layout } from "../layout";
+
+import styles from "../styles/Home.module.scss";
 
 interface HomeProps {}
 
 interface HomeState {}
 
 class Home extends React.Component<HomeProps, HomeState> {
+    redirectCallback = () => {
+        const result = Router.push("/draw");
+    };
+
     render = () => {
-        return <Layout title={"Home"}>Welcome!</Layout>;
+        return (
+            <Layout title={"Home"}>
+                <section className={styles.container}>
+                    <div className={styles.inner}>
+                        <div className={styles.title}>
+                            <h1>포닉스들 중</h1>
+                            <h1>하나를 데려가렴</h1>
+                        </div>
+                        <div className={styles.content}>
+                            <div className={styles.image}>
+                                <Image
+                                    src={"/pokeball.webp"}
+                                    height={"256px"}
+                                    width={"256px"}
+                                    layout={"responsive"}
+                                />
+                            </div>
+                            <button onClick={this.redirectCallback}>포닉씰 뽑으러 가기</button>
+                        </div>
+                    </div>
+                </section>
+            </Layout>
+        );
     };
 }
 
