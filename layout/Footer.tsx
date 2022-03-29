647e6d6acdfa1a777fe70dae1e5eecc2ee846435
beta <beta.underalpha@gmail.com>
2022-03-30 01:51:40 +0900

    (#8) Styling: Footer

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 
+import styles from "./Footer.module.scss";
+
 interface FooterProps {}
 
 interface FooterState {}
 
 class Footer extends React.Component<FooterProps, FooterState> {
     render = () => {
-        return <footer>Footer</footer>;
+        return (
+            <footer className={styles.footer}>
+                <div className={styles.container}>
+                    <div className={styles.inner}>
+                        <div className={styles.title}>
+                            <h1>Ponix Bread</h1>
+                        </div>
+                        <div className={styles.content}>
+                            <p>Powered by Next.js with 💙</p>
+                            <p>
+                                © 2022 Gwon Minjae, Choi Jeongwon —{" "}
+                                <a href={"https://github.com/betarixm/ponix-bread"}>GitHub</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        );
     };
 }
 
